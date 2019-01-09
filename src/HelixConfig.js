23cfc0155125f320f3b116418186cd171cf6a9f5
Tobias Bocanegra <tripodsan@users.noreply.github.com>
2019-01-09 15:27:53 +0900

    Change static directory to `htdocs` #23 (#24)
    fixes #23

--- src/HelixConfig.js
+++ src/HelixConfig.js
@@ -37,7 +37,7 @@ class HelixConfig {
     this._defaults = {
       content: new GitUrl('http://localhost/local/default.git'),
       code: new GitUrl('http://localhost/local/default.git'),
-      staticRoot: '/',
+      staticRoot: '/htdocs',
       directoryIndex: 'index.html',
     };
 
