dbb6c06eb83dcf39d07215918946ac38dc090320
Tobias Bocanegra <tripod@adobe.com>
2020-06-15 14:21:36 +0900

    feat(config): add 'withRepoURL(GitUrl)' to base config

--- src/fetchconfig/fetch.js
+++ src/fetchconfig/fetch.js
@@ -9,54 +9,36 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const URL = require('url');
 const { fetch } = require('@adobe/helix-fetch').context({
   httpsProtocols:
   /* istanbul ignore next */
-  process.env.HELIX_FETCH_FORCE_HTTP1 ? ['http1'] : ['http2', 'http1'],
+    process.env.HELIX_FETCH_FORCE_HTTP1 ? ['http1'] : ['http2', 'http1'],
 });
 const utils = require('../utils');
 const cache = require('./cache');
 
-function computeGithubURI(root, owner, repo, ref, path) {
-  const rootURI = URL.parse(root);
-  const rootPath = rootURI.path;
-  // remove double slashes
-  const fullPath = `${rootPath}/${owner}/${repo}/${ref}/${path}`.replace(
-    /\/+/g,
-    '/',
-  );
-
-  rootURI.pathname = fullPath;
-
-  return URL.format(rootURI);
-}
-
 /**
  * Fetches an FSTab file from a GitHub repository
  * @param {object} opts options
- * @param {string} opts.root base URL for GitHub
- * @param {string} opts.owner GitHub owner or org
- * @param {string} opts.repo GitHub repository
- * @param {string} opts.ref GitHub ref
+ * @param {GitUrl} opts.url the git url of the repo
  * @param {string} opts.name Name of the Config File to fetch
  * @param {object} opts.log Helix-Log instance
  * @param {object} opts.options HTTP request options
  */
 async function fetchConfigUncached(opts) {
   const {
-    root, owner, repo, ref, log, options, name,
+    url, log, options, name,
   } = opts;
-  const response = await fetch(computeGithubURI(root, owner, repo, ref, name), options);
+  const response = await fetch(`${url.raw}/${name}`, options);
   const text = await response.text();
   if (response.ok) {
     return text;
   } else if (response.status === 404) {
-    log.info(`No fstab.yaml found in repo ${owner}/${repo}, ${text}`);
+    log.info(`No ${name} found in repo ${url.owner}/${url.repo}, ${text}`);
     return '';
   }
-  log[utils.logLevelForStatusCode(response.status)](`Invalid response (${response.status}) when fetching ${name} for ${owner}/${repo}`);
-  const err = new Error('Unable to fetch fstab', text);
+  log[utils.logLevelForStatusCode(response.status)](`Invalid response (${response.status}) when fetching ${name} for ${url.owner}/${url.repo}`);
+  const err = new Error(`Unable to fetch ${name}: ${text}`);
   err.status = utils.propagateStatusCode(response.status);
   throw err;
 }
