a810fc2eab90149d54bc0852b1611b4798866cf4
Lars Trieloff <trieloff@adobe.com>
2020-06-23 10:16:37 +0000

    feat(redirects): add dynamic redirects class

---
+++ src/DynamicRedirect.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { fetch } = require('@adobe/helix-fetch');
+
+const DEFAULT_TYPE = 'permanent';
+
+class DynamicRedirect {
+  constructor(src, logger) {
+    this._src = src;
+    this._data = null;
+    this._logger = logger;
+  }
+
+  toJSON() {
+    return this._src;
+  }
+
+  async match(path) {
+    if (!this._data) {
+      try {
+        const res = await fetch(this._src);
+        this._data = await res.json().map();
+      } catch (e) {
+        this._logger.warn(e);
+      }
+    }
+    if (this._data) {
+      const hit = this._data.find((entry) => entry.from === path);
+      return {
+        url: hit.to,
+        type: hit.type || DEFAULT_TYPE,
+      };
+    }
+    return null;
+  }
+}
+
+module.exports = DynamicRedirect;
