141365794d364de44a5a427aefb83cd9b972928f
Tobias Bocanegra <tripod@adobe.com>
2021-09-17 12:52:22 +0200

    chore: cleanup utls

--- packages/helix-shared-ims/test/utils.js
+++ packages/helix-shared-ims/test/utils.js
@@ -12,33 +12,7 @@
 const assert = require('assert');
 const nock = require('nock');
 
-const FSTAB = `
-mountpoints:
-  /: https://drive.google.com/drive/u/2/folders/1vjng4ahZWph-9oeaMae16P9Kbb3xg4Cg
-`;
-
-const DEFAULT_CONTEXT = () => ({
-  log: console,
-  env: {
-    AWS_S3_REGION: 'us-east-1',
-    AWS_S3_ACCESS_KEY_ID: 'fake-key-id',
-    AWS_S3_SECRET_ACCESS_KEY: 'fake-secret',
-  },
-  resolver: {
-    createURL({ package, name, version }) {
-      return new URL(`https://example.com/${package}/${name}@${version}`);
-    },
-  },
-});
-
 function Nock() {
-  const DEFAULT_AUTH = {
-    token_type: 'Bearer',
-    refresh_token: 'dummy',
-    access_token: 'dummy',
-    expires_in: 181000,
-  };
-
   const scopes = {};
 
   let unmatched;
@@ -68,31 +42,7 @@ function Nock() {
     }
   };
 
-  nocker.fstab = (fstab = FSTAB, owner = 'owner', repo = 'repo', ref = 'ref') => nocker('https://helix-code-bus.s3.us-east-1.amazonaws.com')
-    .get(`/${owner}/${repo}/${ref}/fstab.yaml?x-id=GetObject`)
-    .reply(200, fstab);
-
-  nocker.index = (index, owner = 'owner', repo = 'repo', ref = 'ref') => nocker('https://helix-code-bus.s3.us-east-1.amazonaws.com')
-    .get(`/${owner}/${repo}/${ref}/helix-query.yaml?x-id=GetObject`)
-    .reply(200, index);
-
-  nocker.loginWindowsNet = (auth = DEFAULT_AUTH) => nocker('https://login.windows.net')
-    .post('/common/oauth2/token?api-version=1.0')
-    .reply(200, auth);
-
-  nocker.mockIgnore = ({
-    content = '',
-    route = '/adobe/theblog/ignore-tests/.hlxignore',
-    status = 404,
-    times = 1,
-    optional = false,
-  } = {}) => nocker('https://raw.githubusercontent.com')
-    .get(route)
-    .times(times)
-    .optionally(optional)
-    .reply(status, content);
-
   return nocker;
 }
 
-module.exports = { Nock, DEFAULT_CONTEXT };
+module.exports = { Nock };
