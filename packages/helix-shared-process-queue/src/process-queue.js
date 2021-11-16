d480b4dffd65f1a4a3c0c73fbdda067f77a38bd1
Tobias Bocanegra <tripodsan@users.noreply.github.com>
2021-11-16 21:06:06 +0900

    feat: add support for iterables (#600)

--- packages/helix-shared-process-queue/src/process-queue.js
+++ packages/helix-shared-process-queue/src/process-queue.js
@@ -14,29 +14,59 @@
  * Processes the given queue concurrently. The handler functions can add more items to the queue
  * if needed.
  *
- * @param {Array<*>} queue A list of tasks
- * @param {function} fn A handler function `fn(task:any, queue:array, results:array)`
+ * @param {Iterable|Array} queue A list of tasks
+ * @param {ProcessQueueHandler} fn A handler function `fn(task:any, queue:array, results:array)`
  * @param {number} [maxConcurrent = 8] Concurrency level
  * @returns the results
  */
 async function processQueue(queue, fn, maxConcurrent = 8) {
+  if (typeof queue !== 'object') {
+    throw Error('invalid queue argument: iterable expected');
+  }
+
   const running = [];
   const results = [];
-  while (queue.length || running.length) {
-    if (running.length < maxConcurrent && queue.length) {
-      const task = fn(queue.shift(), queue, results);
-      if (task.then) {
-        running.push(task);
-        task.catch(() => {}).finally(() => {
+
+  const handler = (entry) => {
+    const task = fn(entry, queue, results);
+    if (task && task.then) {
+      running.push(task);
+      task
+        .catch(() => {})
+        .finally(() => {
           running.splice(running.indexOf(task), 1);
         });
-      }
-    } else {
-      // eslint-disable-next-line no-await-in-loop
-      await Promise.race(running);
     }
+  };
+
+  // when using array, dequeue the entries
+  if (Array.isArray(queue)) {
+    while (queue.length || running.length) {
+      if (running.length < maxConcurrent && queue.length) {
+        handler(queue.shift());
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        await Promise.race(running);
+      }
+    }
+    return results;
   }
-  return results;
+
+  if ('next' in queue) {
+    let next = queue.next();
+    while (!next.done || running.length) {
+      if (running.length < maxConcurrent && !next.done) {
+        handler(next.value);
+        next = queue.next();
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        await Promise.race(running);
+      }
+    }
+    return results;
+  }
+
+  throw Error('invalid queue argument: iterable expected');
 }
 
 module.exports = processQueue;
