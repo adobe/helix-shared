34c175ebbea3f441d3acc5a9c775bd497ace0aa2
Lars Trieloff <trieloff@adobe.com>
2022-01-04 10:36:53 +0100

    test(bounce): add test cases for both scenarios
    fast enough response, and too slow response.

--- packages/helix-shared-bounce/test/bounce.test.js
+++ packages/helix-shared-bounce/test/bounce.test.js
@@ -18,6 +18,7 @@ process.env.HELIX_FETCH_FORCE_HTTP1 = 'true';
 const assert = require('assert');
 const { Response, Request } = require('@adobe/helix-fetch');
 const wrap = require('@adobe/helix-shared-wrap');
+const nock = require('nock');
 const bounce = require('../src/bounce');
 
 const log = {
@@ -28,14 +29,38 @@ const log = {
 };
 
 describe('Bounce Wrapper Unit Tests', () => {
-  it('Bounces the slow function', async () => {
+  it('Bounces the slow function, but still returns results if it is fast enough', async () => {
+    let fastBounceId;
+    let slowBounceId;
+
     const slowfunction = async (_, context) => new Promise((resolve) => {
-      setTimeout((resolve, 5000, new Response(`ok, job ${context.invocation.bounceId} completed.`)));
+      slowBounceId = context.invocation.bounceId;
+      setTimeout(resolve, 500, new Response(`ok, job ${context.invocation.bounceId} completed.`));
     });
 
-    const fastfunction = async (_, context) => new Response(`I am ready soon, check status at ${context.invocation.bounceId}`);
+    const fastfunction = async (_, context) => {
+      fastBounceId = context.invocation.bounceId;
+      return new Response(`I am ready soon, check status at ${context.invocation.bounceId}`);
+    };
 
-    const actualfunct = wrap(slowfunction).with(bounce, { responder: fastfunction });
+    const actualfunct = wrap(slowfunction).with(bounce, { responder: fastfunction, timeout: 1000 });
+
+    nock('http://localhost').post('/').reply(async function fakeruntime(uri, requestBody) {
+      const request = new Request(`http://localhost${uri}`, {
+        headers: this.req.headers,
+        body: JSON.stringify((requestBody)),
+        method: this.req.method,
+      });
+
+      const response = await actualfunct(request, {
+        log,
+        invocation: {
+        },
+      });
+      const body = await response.text();
+      return [response.status, body];
+    });
+
     const response = await actualfunct(new Request('http://localhost', {
       body: JSON.stringify({ foo: 'bar' }),
       method: 'POST',
@@ -48,5 +73,55 @@ describe('Bounce Wrapper Unit Tests', () => {
       },
     });
     assert.equal(response.status, 200, 'universal function should be executed');
+    assert.equal(slowBounceId, fastBounceId);
+    assert.ok((await response.text()).startsWith('ok, job '));
+  });
+
+  it('Bounces the slow function, returns pro forma response if too slow', async () => {
+    let fastBounceId;
+    let slowBounceId;
+
+    const slowfunction = async (_, context) => new Promise((resolve) => {
+      slowBounceId = context.invocation.bounceId;
+      setTimeout(resolve, 2000, new Response(`ok, job ${context.invocation.bounceId} completed.`));
+    });
+
+    const fastfunction = async (_, context) => {
+      fastBounceId = context.invocation.bounceId;
+      return new Response(`I am ready soon, check status at ${context.invocation.bounceId}`);
+    };
+
+    const actualfunct = wrap(slowfunction).with(bounce, { responder: fastfunction });
+
+    nock('http://localhost').post('/').reply(async function fakeruntime(uri, requestBody) {
+      const request = new Request(`http://localhost${uri}`, {
+        headers: this.req.headers,
+        body: JSON.stringify((requestBody)),
+        method: this.req.method,
+      });
+
+      const response = await actualfunct(request, {
+        log,
+        invocation: {
+        },
+      });
+      const body = await response.text();
+      return [response.status, body];
+    });
+
+    const response = await actualfunct(new Request('http://localhost', {
+      body: JSON.stringify({ foo: 'bar' }),
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    }), {
+      log,
+      invocation: {
+      },
+    });
+    assert.equal(response.status, 200, 'universal function should be executed');
+    assert.equal(slowBounceId, fastBounceId);
+    assert.ok((await response.text()).startsWith('I am ready soon, check status at '));
   });
 });
