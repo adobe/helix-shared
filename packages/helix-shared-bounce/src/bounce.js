da0797b7c3658bca2b0c3cd04f32fcf2f47839a6
Lars Trieloff <trieloff@adobe.com>
2022-02-07 16:05:15 +0100

    fix(bounce): re-build request from `request.init`

--- packages/helix-shared-bounce/src/bounce.js
+++ packages/helix-shared-bounce/src/bounce.js
@@ -53,7 +53,8 @@ function bounce(func, { responder, timeout = 500 }) {
     const signal = timeoutSignal(2 * timeout);
     const actualResponse = (async () => {
       try {
-        return await fetch(request, {
+        return await fetch(request.url, {
+          ...request.init,
           signal,
         });
       } catch (e) {
