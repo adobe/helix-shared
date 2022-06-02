5f095ef276e1a44a36d9f3168d25ecf1074fa4ad
Dominique Pfister <dpfister@adobe.com>
2022-06-02 14:43:34 +0200

    fix: allow adding sitemap or languages (#690)
    * fix: allow adding sitemap or languages
    
    * fix: initialize languages in sitemap

--- packages/helix-shared-config/src/SitemapConfig.js
+++ packages/helix-shared-config/src/SitemapConfig.js
@@ -41,6 +41,74 @@ class SitemapConfig extends SchemaDerivedConfig {
     this._version = this._cfg.version;
     return this;
   }
+
+  /**
+   * Adds a sitemap definition.
+   *
+   * @param {Object} sitemap sitemap configuration
+   * @param {string} sitemap.name sitemap name
+   * @param {string} sitemap.origin sitemap origin
+   * @param {string} sitemap.source sitemap source
+   * @param {string} sitemap.destination sitemap destination
+   * @param {string} sitemap.lastmod lastmod format
+   * @return new sitemap
+   */
+  addSitemap({
+    name, origin, source, destination, lastmod,
+  }) {
+    const { sitemaps } = this._cfg;
+    if (sitemaps[name]) {
+      throw new Error(`Unable to add sitemap definition with existing name: ${name}`);
+    }
+    sitemaps[name] = {
+      origin,
+      source,
+      destination,
+      lastmod,
+    };
+
+    // let BaseConfig.toYAML() use the JSON output
+    this._document = null;
+    return sitemaps[name];
+  }
+
+  /**
+   * Adds a language definition within a sitemap.
+   *
+   * @param {string} sitemapName sitemap name
+   * @param {Object} language language configuration
+   * @param {string} language.name language name
+   * @param {string} language.source language source
+   * @param {string} language.destination language destination
+   * @param {string} language.hreflang href language
+   * @param {string} language.alternate alternate location of this language
+   * @return new language
+   */
+  addLanguage(sitemapName, {
+    name, source, destination, hreflang, alternate,
+  }) {
+    const { sitemaps } = this._cfg;
+
+    const sitemap = sitemaps[sitemapName];
+    if (!sitemap) {
+      throw new Error(`Unable to add language, sitemap not found: ${sitemapName}`);
+    }
+    // eslint-disable-next-line no-multi-assign
+    const languages = (sitemap.languages = sitemap.languages || {});
+    if (languages[name]) {
+      throw new Error(`Unable to add language definition with existing name: ${name}`);
+    }
+    languages[name] = {
+      source,
+      destination,
+      hreflang,
+      alternate,
+    };
+
+    // let BaseConfig.toYAML() use the JSON output
+    this._document = null;
+    return languages[name];
+  }
 }
 
 module.exports = SitemapConfig;
