987095b2b8e5e23954b70398e116b9b10ef9a1c4
Tobias Bocanegra <tripod@adobe.com>
2024-01-18 11:25:26 +0100

    chore: fix semantic release (again)

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "extends": "semantic-release-monorepo",
+  extends: "semantic-release-monorepo",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
@@ -11,7 +11,7 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {}],
   ],
   branches: ['main'],
 };
