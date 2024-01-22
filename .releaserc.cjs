a13342b8fffbdc55901027944a01f069adff14b6
Tobias Bocanegra <tripod@adobe.com>
2024-01-22 23:45:13 +0100

    chore: fix release.cjs [skip ci]

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     }],
     "@semantic-release/npm",
     ["@semantic-release/git", {
-      "assets": ["package.json", "CHANGELOG.md"],
+      "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     ["@semantic-release/github", {}],
