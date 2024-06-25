828bb6da05d09ffedab6c2c37c789c84cd2383d8
Dominique Pfister <dpfister@adobe.com>
2024-06-25 20:03:20 +0200

    chore(test): fix title

--- packages/helix-shared-tokencache/test/s3-cache-plugin.test.js
+++ packages/helix-shared-tokencache/test/s3-cache-plugin.test.js
@@ -214,7 +214,7 @@ describe('S3CachePlugin Test', () => {
     assert.strictEqual(ret, false);
   });
 
-  it('does not write data to s3 if contents has no Account keys', async () => {
+  it('does not write data to s3 if Account is empty', async () => {
     const p = new S3CachePlugin({
       bucket: 'test-bucket',
       key: 'myproject/auth-default/json',
