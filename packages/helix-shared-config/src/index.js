4a965e5ceb7767d92de2dda94776ae84755160df
Dominique Pfister <dpfister@adobe.com>
2024-07-02 15:00:43 +0200

    fix: export index and sitemap schemas (#965)

--- packages/helix-shared-config/src/index.js
+++ packages/helix-shared-config/src/index.js
@@ -9,6 +9,14 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+import indexSchema from './schemas/index.schema.cjs';
+import indexConfigSchema from './schemas/indexconfig.schema.cjs';
+import propertySchema from './schemas/property.schema.cjs';
+import querySchema from './schemas/query.schema.cjs';
+import sitemapLanguageSchema from './schemas/sitemap-language.schema.cjs';
+import sitemapSchema from './schemas/sitemap.schema.cjs';
+import sitemapConfigSchema from './schemas/sitemapconfig.schema.cjs';
+
 export { IndexConfig } from './IndexConfig.js';
 export { MountConfig } from './MountConfig.js';
 export { optionalConfig, requiredConfig } from './config-wrapper.js';
@@ -17,3 +25,13 @@ export { ValidationError } from './ValidationError.js';
 export { IgnoreConfig } from './IgnoreConfig.js';
 export { SitemapConfig } from './SitemapConfig.js';
 export { ModifiersConfig } from './ModifiersConfig.js';
+
+export const SCHEMAS = [
+  indexSchema,
+  indexConfigSchema,
+  propertySchema,
+  querySchema,
+  sitemapLanguageSchema,
+  sitemapSchema,
+  sitemapConfigSchema,
+];
